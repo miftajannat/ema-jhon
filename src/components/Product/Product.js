b34b1754ca63079a9566e2cf7062414a604e3eb9
miftajannat <miftajannat.dev@gmail.com>
2022-12-14 18:52:11 +0600

    order summary

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,7 +6,7 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 
 const Product = (props) => {
-    console.log(props.product);
+    //console.log(props);
     const { img, name, seller, price, stock } = props.product;
     return (
         <div className='product'>
@@ -20,7 +20,11 @@ const Product = (props) => {
                 <p><small>by:{seller} </small></p>
                 <p>${price}</p>
                 <p><small>Only {stock} left in stock - Order soon</small></p>
-                <button className='main-button'><FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>
+                <button
+                    className='main-button'
+                    onClick={() => props.handleAddProduct(props.product)}
+                >
+                    <FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>
             </div>
 
         </div>
